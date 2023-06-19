4a543691baf92564af6702f9c883dbab9ab3948d
Zhi <neozhixuan@gmail.com>
2023-06-19 18:59:08 +0800

    .

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import app from "./server.js";
-import mongodb from "mongodb";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import ReferralsDAO from "./dao/referralDAO.js";
 import orgService from "./api/orgService.js";
-import mongoose from "mongoose";
 import cors from "cors";
+
 dotenv.config();
 const port = process.env.PORT || 8000;
 
@@ -14,7 +14,7 @@ mongoose
     useUnifiedTopology: true,
     dbName: "Backend", // Set the desired database name here
   })
-  .then(() => console.log("MongoDB is  connected successfully"))
+  .then(() => console.log("MongoDB is connected successfully"))
   .catch((err) => {
     console.error(err.stack);
     process.exit(1);
@@ -26,38 +26,10 @@ mongoose
     app.use(cors());
 
     // How we start the web server after connecting
-    app.listen(port, () => {
+    const server = app.listen(port, () => {
       // Log the process
       console.log(`listening on port ${port}`);
     });
+
+    // Export the server for Vercel deployment
   });
-
-// dotenv.config();
-// // Access mongo client
-// const MongoClient = mongodb.MongoClient;
-
-// // 8000 if the current one cannot be accessed
-// const port = process.env.PORT || 8000;
-
-// MongoClient.connect(process.env.REFERRALS_DB_URI, {
-//   // options: max people who can connnect
-//   maxPoolSize: 50,
-//   // after x time, the request times out
-//   wtimeoutMS: 250,
-//   // Mongodb nodejs driver rewrote the tool that parses mongodb
-//   // connection strings
-//   useNewUrlParser: true,
-// })
-//   .catch((err) => {
-//     console.error(err.stack);
-//     process.exit(1);
-//   })
-//   .then(async (client) => {
-//     await ReferralsDAO.injectDB(client);
-//     await orgService.injectDB(client);
-//     // How we start the web server after connecting
-//     app.listen(port, () => {
-//       // Log the process
-//       console.log(`listening on port ${port}`);
-//     });
-//   });
