1e7e21123b28d474c95cb12a8fa24937e56681f4
Zhi <neozhixuan@gmail.com>
2023-06-20 14:31:06 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your desired origin URL
+    origin: "https://referr.vercel.app", // Replace with your desired origin URL
     credentials: true,
   })
 );
