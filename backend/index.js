867486ab7f3d4757b17cf859ad061fbeea038a64
Zhi <neozhixuan@gmail.com>
2023-06-20 12:07:04 +0800

    Test

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,20 @@
 import app from "./server.js";
-import mongoose from "mongoose";
+import mongodb from "mongodb";
 import dotenv from "dotenv";
 import ReferralsDAO from "./dao/referralDAO.js";
 import orgService from "./api/orgService.js";
+import mongoose from "mongoose";
 import cors from "cors";
-import express from "express";
-import referrals from "./api/referrals.route.js";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
 dotenv.config();
-
-const app = express();
-
-app.use(
-  cors({
-    origin: "http://localhost:3000", // Replace with your desired origin URL
-    credentials: true,
-  })
-);
-
-// Server can accept json in request body
-app.use(express.json());
-app.use(cookieParser());
-app.use("/api/v1", referrals);
-// If you go into a route that is not in the file
-app.use("*", (req, res, next) => {
-  if (!res.headersSent) {
-    res.status(404).json({ error: "Not found" });
-  } else {
-    next();
-  }
-});
-
 const port = process.env.PORT || 8000;
-if (process.env.NODE_ENV !== "production") {
-  dotenv.config({ path: __dirname + "/.env" });
-}
+
 mongoose
   .connect(process.env.REFERRALS_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: "Backend", // Set the desired database name here
+    dbName: process.env.REFERRALS_NS,
   })
-  .then(() => console.log("MongoDB is connected successfully"))
+  .then(() => console.log("MongoDB is  connected successfully"))
   .catch((err) => {
     console.error(err.stack);
     process.exit(1);
@@ -58,16 +30,6 @@ mongoose
       // Log the process
       console.log(`listening on port ${port}`);
     });
-    app.use("/", require(path.join(__dirname, "api", "v1")));
-    if (process.env.NODE_ENV === "production") {
-      app.use(express.static(path.join(__dirname, "../frontend", "build")));
-      app.get("/*", (req, res) => {
-        res.sendFile(
-          path.join(__dirname, "../frontend", "build", "index.html")
-        );
-      });
-    }
-    // Export the server for Vercel deployment
   });
 
-export default app;
+export default app; // Exporting the app object
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import referrals from "./api/referrals.route.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Replace with your desired origin URL
+    credentials: true,
+  })
+);
+
+// Server can accept json in request body
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/v1", referrals);
+// If you go into a route that is not in the file
+app.use("*", (req, res, next) => {
+  if (!res.headersSent) {
+    res.status(404).json({ error: "Not found" });
+  } else {
+    next();
+  }
+});
+// So we can import in a file that accesses the database
+export default app;
