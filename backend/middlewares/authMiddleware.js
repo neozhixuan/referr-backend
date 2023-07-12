a0595b939a1411c153b0a9aff6644b0456c07628
Zhi <neozhixuan@gmail.com>
2023-07-12 11:56:03 +0800

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,13 +4,17 @@ import jwt from "jsonwebtoken";
 const userVerification = (req, res) => {
   const token = req.cookies.token;
   if (!token) {
+    console.log("No token found");
     return res.json({ status: false });
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
+      console.log("Error with verifying token");
       return res.json({ status: false });
     } else {
       const user = await User.findById(data.id);
+      console.log("Supposed user: ", user);
+
       if (user) return res.json({ status: true, user: user.username });
       else return res.json({ status: false });
     }
