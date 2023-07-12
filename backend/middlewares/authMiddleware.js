6c831dd83adb7b797fdb44e9329f4a6a7d7f5e5d
Zhi <neozhixuan@gmail.com>
2023-07-12 12:08:37 +0800

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,19 +4,25 @@ import jwt from "jsonwebtoken";
 const userVerification = (req, res) => {
   const token = req.cookies.token;
   if (!token) {
-    console.log("No token found");
-    return res.json({ status: false });
+    return res.json({ status: false, message: "No token found" });
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
       console.log("Error with verifying token");
-      return res.json({ status: false });
+      return res.json({
+        status: false,
+        message: "Error with verifying token",
+        error: err,
+      });
     } else {
       const user = await User.findById(data.id);
-      console.log("Supposed user: ", user);
 
       if (user) return res.json({ status: true, user: user.username });
-      else return res.json({ status: false });
+      else
+        return res.json({
+          status: false,
+          message: "User not found in database",
+        });
     }
   });
 };
